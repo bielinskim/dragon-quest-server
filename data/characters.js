e54c5dc0dfef190a31e5bf04a3fc047c7f521b36
mb77867 <mb77867@stud.uph.edu.pl>
2022-02-09 23:53:53 +0100

    init

---
+++ data/characters.js
@@ -0,0 +1,6 @@
+const characters = [{id: 1, name: "Abu", experience: 0, imageId: 1},
+{id: 2, name: "Praveen", experience: 1200, imageId: 2},
+{id: 3, name: "Sathya", experience: 13999, imageId: 3},
+{id: 4, name: "Ailen", experience: 38000, imageId: 4}];
+
+module.exports = characters;
---
+++ dist/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const quests = require('../data/quests');
+
+const characters = require('../data/characters');
+
+app.get('/', (req, res) => {
+  res.json([{
+    id: 1,
+    level: 2,
+    minExp: 3,
+    maxExp: 4
+  }]);
+});
+app.get('/quest', (req, res) => {
+  res.json(quests);
+});
+app.get('/character', (req, res) => {
+  res.json(characters);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
